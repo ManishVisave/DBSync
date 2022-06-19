6066d8fb44e725ab295a6d3fdf6feb6fd24ebd79
Manish Visave <manish.visave@acquia.com>
2022-06-19 12:01:16 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='DBSyncTool',
-    version='1.0.2',
+    version='1.0.3',
     long_description=README,
     long_description_content_type="text/markdown",
     packages=['dbsync'],
